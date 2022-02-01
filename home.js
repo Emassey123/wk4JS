b98c37e49a3d8961684858a971be34c7eb68dbeb
Emassey123 <94918775+Emassey123@users.noreply.github.com>
2022-02-01 08:00:22 -0500

    updating files

--- home.js
+++ home.js
@@ -45,3 +45,49 @@ let array1 = [4, 5, 6, 0, 1, 2];
 
 console.log(targretValChecker(array1, 5));
 
+// Very Hard 
+
+// You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+// Hint:  Sudo code this problem. Focus on breaking the problem down into steps  Use functions, arrays and logical operators.  Do not have anyone give you the answer or solve this problem for you. 
+
+// Example 1:
+
+// Input: coins = [1, 2, 5], amount = 11
+
+// Output: 3 
+
+// Explanation: 11 = 5 + 5 + 1
+
+// Example 2:
+
+// Input: coins = [2], amount = 3
+
+// Output: -1
+
+// sort coins from largest to smallest
+// three quarters = 75 
+// 96-75=21
+// 2 dimes = 20
+// 21-20=1
+// 1 penny = 1cent 
+// [25,10,1]      amount = 96
+
+const coinChecker = (arr, amount) => {
+   sortedArray = arr.sort().reverse();
+//    [25,10,1]
+    //    assingning sum to 0
+    let sum = 0;
+    
+    // looping through the array
+   for(i = 0; i < arr.length; i++){   
+    //  at first arr[i] = 25 
+     sum += Math.floor(amount / arr[i])
+    //  sum = 96/25 on the first loop and is rounded down to = "3"
+    // sum = 96/25 on the first loop and is rounded down to = "3"
+    // sum = 96/25 on the first loop and is rounded down to = "3"
+   }
+   return sum
+}
+const array3 = [25,10,1];  // []
+console.log(coinChecker(array3, 96));
